985d281ff8c84a5609c482eeaf69bf55e307c9a5
Conservation Innovation Center <johnsmithcic1607@gmail.com>
2017-09-08 20:56:09 -0400

    Update spreadsheet-basics.py

--- spreadsheet-basics.py
+++ spreadsheet-basics.py
@@ -1,20 +1,20 @@
 ## Import libraries
-import os               ## short for "operating system"... lots of utility functions
+import os   ## short for "operating system"... lots of utility functions
 
 ## Create path to data and set it as our working directory
 dataDirectory = '/Users/colin/Desktop/chescon-github/pandas/sample-data'
 os.chdir(dataDirectory)
 
-## Open Excel file
+## Open csv file
 hucs = open('huc_table.csv')
 
 ## Get comma separated string of columns
 header = hucs.readline()
-print(type(header))       ## Prove this is a string
+print(type(header))     ## Prove this is a string
 
 ## Create a comma separated list of columns
 columns = header.split(",")
-print(type(columns))       ## Prove this is a list
+print(type(columns))    ## Prove this is a list
 
 ## Loop through the list and print the names
 for column in columns:
